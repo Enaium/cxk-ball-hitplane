f65a6dc8e29d141675ee925f48c5cfcaf90ebdb8
Lightcolour <32991121+Lightcolour-666@users.noreply.github.com>
2019-05-06 16:09:12 +0800

    增加PC浏览检测
    增加PC浏览检测

--- js/main.js
+++ js/main.js
@@ -19,7 +19,20 @@ var kill=document.getElementById("sound-kill");
     //游戏结束
 var gameover=document.getElementById("sound-gameover");
 
-
+    //检测是否为PC浏览
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
+        alert("检测到为PC浏览\n打开影响式设计模式（Ctrl+Shift+M ）正常浏览（最好选iphone X/XS）")
+    }
+    
 /*
  创建飞机类
  */
