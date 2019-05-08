f606dc815c9c5c20f5bf8f5eb4525e4d8d2ae357
Lightcolour <32991121+Lightcolour-666@users.noreply.github.com>
2019-05-08 11:15:19 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -379,7 +379,7 @@ function start(){
                       //碰撞本方飞机，游戏结束，统计分数
                       selfplan.imagenode.src="image/本方飞机爆炸.gif";
 
-                      this.gameover.get(0).play();
+                      this.gameover.play();
 
                       enddiv.style.display="block";
                       planscore.innerHTML=scores;
